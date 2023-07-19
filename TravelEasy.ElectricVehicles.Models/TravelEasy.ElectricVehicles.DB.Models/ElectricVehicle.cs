c2a8f7c5be93ee7a1440234d351c76ddf6947ce3
Anton Dimitrov <antonvd680@gmail.com>
2023-07-19 02:12:54 -0700

    + updated EV model + added user model

--- TravelEasy.ElectricVehicles.Models/TravelEasy.ElectricVehicles.DB.Models/ElectricVehicle.cs
+++ TravelEasy.ElectricVehicles.Models/TravelEasy.ElectricVehicles.DB.Models/ElectricVehicle.cs
@@ -1,17 +1,25 @@
-﻿namespace TravelEasy.ElectricVehicles.DB.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TravelEasy.ElectricVehicles.DB.Models
 {
     public class ElectricVehicle
     {
+        [Key]
         public int Id { get; set; }
 
+        [Required]
         public string? Brand { get; set; }
 
+        [Required]
         public string? Model { get; set; }
 
+        [Required]
         public int HorsePowers { get; set; }
 
+        [Required]
         public int Range { get; set; }
 
+        [Required]
         public decimal PricePerDay { get; set; }
     }
 }
