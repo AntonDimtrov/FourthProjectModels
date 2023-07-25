57b112129aafd2e5313c98fb085f3ed5aa3f589a
Anton Dimitrov <antonvd680@gmail.com>
2023-07-25 08:09:15 -0700

    + added Category to EV model + switched NuGet package

--- TravelEasy.ElectricVehicles.Models/TravelEasy.ElectricVehicles.DB.Models/ElectricVehicle.cs
+++ TravelEasy.ElectricVehicles.Models/TravelEasy.ElectricVehicles.DB.Models/ElectricVehicle.cs
@@ -4,25 +4,28 @@ namespace TravelEasy.ElectricVehicles.DB.Models
 {
     public class ElectricVehicle
     {
-        [Key]
-        public int Id { get; set; }
+            [Key]
+            public int Id { get; set; }
 
-        [Required]
-        public string? Brand { get; set; }
+            [Required]
+            public string? Brand { get; set; }
 
-        [Required]
-        public string? Model { get; set; }
+            [Required]
+            public string? Model { get; set; }
 
-        [Required]
-        public int HorsePowers { get; set; }
+            [Required]
+            public int HorsePowers { get; set; }
 
-        [Required]
-        public int Range { get; set; }
+            [Required]
+            public int Range { get; set; }
 
-        [Required]
-        public decimal PricePerDay { get; set; }
+            [Required]
+            public decimal PricePerDay { get; set; }
 
-        [Required]
-        public string? ImageURL { get; set; }
+            [Required]
+            public string? ImageURL { get; set; }
+
+            [Required]
+            public string? Category { get; set; }
     }
 }
